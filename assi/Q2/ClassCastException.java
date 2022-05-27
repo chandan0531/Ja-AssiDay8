e5d1e0a784aa760af71587cf7e1b4d1da3a286f5
chandan0531 <chandan0521@gmail.com>
2022-05-27 22:15:11 +0530

    Day8assi

---
+++ assi/Q2/ClassCastException.java
@@ -0,0 +1,12 @@
+//ClassCastException :
+//We can downcast the parent class variable to the child class object only if
+//the Parent class variable points to the Child class object , otherwise it will throw a
+//runtime exception called ClassCastException.
+
+package com.assi.Q2;
+
+public class ClassCastException {
+	public static void main(String[] args) {
+		System.out.println("ClassCastException");
+	}
+}
